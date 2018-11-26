a5595930914ca8a2c110b69184f39275e943dfbd
Jared <jhbarnum@gmail.com>
2018-11-26 06:34:14 -0500

    conditionals

--- src/App.js
+++ src/App.js
@@ -11,8 +11,15 @@ class App extends Component {
       {name: 'Jared', username: 'jbarnum'},
       { name: 'Paragraph 1' },
       { name: 'Paragraph 2' }
-    ]
+    ],
+      showPersons: false
   }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
   newUserNameHandler = (event) => {
     this.setState({
       userinput: [
@@ -44,16 +51,36 @@ class App extends Component {
 
     return (
       <div className="App">
-      <button style={style} onClick={() => this.userNameHandler('JB')}>Change UserName</button>
-       <UserInput 
-       name={this.state.userinput[0].name} 
-       changed={this.newUserNameHandler}/>
-       <UserOutput
-       name={this.state.userinput[1].name}/>
-       <UserOutput
-       name={this.state.userinput[2].name}/>
-       <UserOutput
-       username={this.state.userinput[0].username}/>
+        <button style={style}
+          onClick={this.togglePersonsHandler}>Toggle People</button>
+      { 
+        this.state.showPersons ?
+        <div>
+          <UserInput
+            name={this.state.userinput[0].name}
+            changed={this.newUserNameHandler} />
+          <UserOutput
+            name={this.state.userinput[1].name} />
+          <UserOutput
+            name={this.state.userinput[2].name} />
+          <UserOutput
+            username={this.state.userinput[0].username} />
+        </div> 
+        : null
+      }
+      <button style={style}
+      onClick={() => this.userNameHandler('JB')}>Change UserName</button>
+          <div>
+              <UserInput 
+              name={this.state.userinput[0].name} 
+              changed={this.newUserNameHandler}/>
+              <UserOutput
+              name={this.state.userinput[1].name}/>
+              <UserOutput
+              name={this.state.userinput[2].name}/>
+              <UserOutput
+              username={this.state.userinput[0].username}/>
+          </div>
       </div>
     );
   }
